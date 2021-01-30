ac926018669e542925fe6d950e70c6ca9dbdb102
hyq <965672903@qq.com>
2021-01-30 18:43:12 +0800

    认证中心 搭建

---
+++ zephyr-auth/src/main/java/cn/hyqup/auth/AuthApplication.java
@@ -0,0 +1,20 @@
+package cn.hyqup.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Copyright © 2021灼华. All rights reserved.
+ *
+ * @author create by hyq
+ * @version 1.0
+ * @date 2021/1/30
+ * @description:
+ */
+@SpringBootApplication
+public class AuthApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthApplication.class);
+    }
+
+}
