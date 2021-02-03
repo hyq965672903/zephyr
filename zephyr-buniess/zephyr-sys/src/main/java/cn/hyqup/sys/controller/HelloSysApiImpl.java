00fb623d536444cc7e0dc9232ee4a56e0a18ea91
hyq <965672903@qq.com>
2021-02-03 20:13:21 +0800

    feat: sentinel 熔断

--- zephyr-buniess/zephyr-sys/src/main/java/cn/hyqup/sys/controller/HelloSysApiImpl.java
+++ zephyr-buniess/zephyr-sys/src/main/java/cn/hyqup/sys/controller/HelloSysApiImpl.java
@@ -1,6 +1,8 @@
 package cn.hyqup.sys.controller;
 
 import cn.hyqup.sys.api.HelloSysApi;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,10 +14,15 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2021/1/31
  * @description:
  */
+@Slf4j
 @RestController
 public class HelloSysApiImpl implements HelloSysApi {
+//    @Autowired
+//    MyProperties myProperties;
+
     @Override
     public String saySysHello() {
+//        log.info(myProperties.toString());
 //        int i= 1/0;
         return "hello saySysHello";
     }
---
+++ zephyr-buniess/zephyr-sys/src/main/java/cn/hyqup/sys/properties/MyProperties.java
@@ -0,0 +1,21 @@
+//package cn.hyqup.sys.properties;
+//
+//import lombok.Data;
+//import org.springframework.boot.context.properties.ConfigurationProperties;
+//import org.springframework.stereotype.Component;
+//
+///**
+// * Copyright © 2021灼华. All rights reserved.
+// *
+// * @author create by hyq
+// * @version 1.0
+// * @date 2021/2/2
+// * @description:
+// */
+//@Data
+//@Component
+//@ConfigurationProperties(prefix = "user")
+//public class MyProperties {
+//    private String name;
+//    private String age;
+//}
